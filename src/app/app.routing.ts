ee24635dcc8ba045d13e7033e4524f45d9c7231b
Roman Yepifanov <31592494+yero13@users.noreply.github.com>
2018-03-10 00:50:44 +0200

    refactoring - tabs

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 export const appRoutes: Routes = [
-  { path: '',   redirectTo: '/resalloc', pathMatch: 'full' }
+  { path: '',   redirectTo: '/gantt', pathMatch: 'full' }
   // { path: '**', component: PageNotFoundComponent }
 ];
 
