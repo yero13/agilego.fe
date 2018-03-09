9e072adf26880cc37503e9d0e850b90a4c6d7262
Roman Yepifanov <31592494+yero13@users.noreply.github.com>
2018-03-10 00:48:31 +0200

    refactoring - tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'agilego';
+
+  routeLinks: any[];
+  activeLinkIndex = 0;
+  constructor(private router: Router) {
+    this.routeLinks = [
+      {label: 'Gantt', link: 'gantt'},
+      {label: 'Resource allocation', link: 'resalloc'}];
+  }
 }
